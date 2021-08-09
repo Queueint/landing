3f8042629b08e3d6efe187ba5efb3101989205f5
Climax708 <soryy708@gmail.com>
2021-08-09 23:57:52 +0300

    Implement new header

--- index.js
+++ index.js
@@ -11,18 +11,66 @@ ScrollReveal().reveal('.iconContainer', {
 
 window.addEventListener('load', () => {
   const nav = document.getElementsByClassName('nav')[0];
-  const navButton = document.querySelector('.nav .btn');
-  navButton.addEventListener('click', clickInsideEvent => {
+  const navBurgerButton = document.querySelector('.nav .burger');
+  navBurgerButton.addEventListener('click', () => {
     nav.classList.toggle('active');
-
-    const onClickOutside = clickOutsideEvent => {
-      if (clickInsideEvent !== clickOutsideEvent) {
-        nav.classList.remove('active');
-        document.removeEventListener('click', onClickOutside);
-      }
-    };
-    document.addEventListener('click', onClickOutside);
   });
+  const navCloseButton = document.querySelector('.nav .close');
+  navCloseButton.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+  const navItems = document.querySelectorAll('.nav .nav-item');
+  navItems.forEach(navItem => {
+    if (!navItem.classList.contains('select')) {
+      navItem.addEventListener('click', () => {
+        nav.classList.remove('active');
+      });
+    }
+  });
+
+  const allSelects = document.querySelectorAll('.select');
+  allSelects.forEach(selectElement => {
+    selectElement.addEventListener('click', () => {
+      selectElement.classList.toggle('active');
+    });
+  });
+
+  const handleScrollY = () => {
+    const header = document.getElementsByClassName('header')[0];
+    const body = document.getElementsByClassName('body')[0];
+    if (window.scrollY > 48) {
+      header.classList.add('scrolled');
+      body.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+      body.classList.remove('scrolled');
+    }
+
+    const links = document.querySelectorAll('.nav a');
+    let foundCurrent = false;
+    links.forEach((link, i) => {
+      const href = link.getAttribute('href');
+      if (!href.startsWith('#')) {
+        return;
+      }
+      const target = document.querySelector(href);
+      const targetY = target.offsetTop - 1;
+      if (window.scrollY > targetY) {
+        link.classList.add('current');
+        foundCurrent = true;
+        if (i > 0) {
+          links[i - 1].classList.remove('current');
+        }
+      } else {
+        link.classList.remove('current');
+      }
+    });
+    if (!foundCurrent) {
+      links[0].classList.add('current');
+    }
+  };
+  document.addEventListener('scroll', handleScrollY);
+  handleScrollY();
 });
 
 function emitEvent(event) {
