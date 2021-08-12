ed304f581aee708103cee4b0c449ee9dca0a47aa
Climax708 <soryy708@gmail.com>
2021-08-12 20:57:23 +0300

    Implement form validation

--- index.js
+++ index.js
@@ -110,34 +110,215 @@ window.addEventListener('load', () => {
   window.addEventListener('resize', handleViewportChange);
   handleViewportChange();
 
+  const firstNameElement = document.getElementById('contact-form-first-name');
+  const lastNameElement = document.getElementById('contact-form-last-name');
+  const emailElement = document.getElementById('contact-form-email');
+  const messageElement = document.getElementById('contact-form-message');
+  const formFields = [firstNameElement, lastNameElement, emailElement, messageElement];
+  const statusMessageElement = document.getElementById('contact-form-status-message');
+  const submitButton = document.getElementById('contact-form-submit');
+  
+  const validationErrors = {
+    firstName: false,
+    lastName: false,
+    email: false,
+    message: false,
+  };
+
+  const formIsInvalid = () => Object.values(validationErrors).reduce((prev, cur) => prev || cur);
+  const makeInputErrored = element => element.closest('.input-container').classList.add('errored');
+
+  const handleSubmitButton = () => {
+    const requiredFormFields = formFields.filter(field => field !== messageElement);
+    const formLacksRequired = () => requiredFormFields
+      .map(field => field.value.trim())
+      .reduce((prev, cur) => prev || cur === '', false);
+    
+    if (formIsInvalid() || formLacksRequired()) {
+      submitButton.disabled = true;
+    } else {
+      submitButton.disabled = false;
+    }
+  };
+  handleSubmitButton();
+
+  const handleValidation = () => {
+    const isString = (s) => typeof s === 'string' || s instanceof String;
+    const isEmail = (s) => isString(s) && /^.+@.+\..+$/u.test(s);
+    const makeInputNotErrored = element => element.closest('.input-container').classList.remove('errored');
+  
+    const updateErrorMessage = () => {
+      const setMessage = (text) => statusMessageElement.innerText = text;
+      const setErrorMessage = (errorKey) => setMessage(statusMessageElement.getAttribute(errorKey));
+  
+      let errored = false;
+      if (validationErrors.firstName || validationErrors.lastName) {
+        setErrorMessage('data-name-validation-text');
+        errored = true;
+      }
+      if (validationErrors.email) {
+        setErrorMessage('data-email-validation-text');
+        errored = true;
+      }
+      if (validationErrors.message) {
+        setErrorMessage('data-message-validation-text');
+        errored = true;
+      }
+      if (errored) {
+        statusMessageElement.classList.add('error');
+      } else {
+        statusMessageElement.classList.remove('error');
+        setMessage('');
+      }
+      handleSubmitButton();
+    };
+  
+    firstNameElement.addEventListener('input', () => {
+      const value = firstNameElement.value.trim();
+      const isInvalid = !value || !isString(value) || !/^[a-zA-Zא-ת ]+$/u.test(value);
+      if (isInvalid) {
+        makeInputErrored(firstNameElement)
+        validationErrors.firstName = true;
+      } else {
+        makeInputNotErrored(firstNameElement)
+        validationErrors.firstName = false;
+      }
+      updateErrorMessage();
+    });
+  
+    lastNameElement.addEventListener('input', () => {
+      const value = lastNameElement.value.trim();
+      const isInvalid = !value || !isString(value) || !/^[א-תa-zA-Z ]+$/u.test(value);
+      if (isInvalid) {
+        makeInputErrored(lastNameElement)
+        validationErrors.lastName = true;
+      } else {
+        makeInputNotErrored(lastNameElement)
+        validationErrors.lastName = false;
+      }
+      updateErrorMessage();
+    });
+  
+    emailElement.addEventListener('input', () => {
+      const value = emailElement.value.trim();
+      const isInvalid = !value || !isEmail(value);
+      if (isInvalid) {
+        makeInputErrored(emailElement)
+        validationErrors.email = true;
+      } else {
+        makeInputNotErrored(emailElement)
+        validationErrors.email = false;
+      }
+      updateErrorMessage();
+    });
+  
+    messageElement.addEventListener('input', () => {
+      const value = messageElement.value.trim();
+      const isInvalid = value && (!isString(value) || !/^[א-תa-zA-Z0-9 \n\r,.!?@#$%^&*():\\/\[\]'"=+_`~\-]+$/u.test(value));
+      if (isInvalid) {
+        makeInputErrored(messageElement)
+        validationErrors.message = true;
+      } else {
+        makeInputNotErrored(messageElement)
+        validationErrors.message = false;
+      }
+      updateErrorMessage();
+    });
+  };
+  handleValidation();
+
   const contactForm = document.getElementById('contact-form');
   contactForm.addEventListener('submit', e => {
     const formTargetUrl = 'https://api.q-int.com/contact';
 
-    const getValues = () => {
-      const firstNameElement = document.getElementById('contact-form-first-name');
-      const lastNameElement = document.getElementById('contact-form-last-name');
-      const emailElement = document.getElementById('contact-form-email');
-      const messageElement = document.getElementById('contact-form-message');
-      return {
-        firstName: firstNameElement.value,
-        lastName: lastNameElement.value,
-        emailAddress: emailElement.value,
-        message: messageElement.value,
-      };
-    };
-
     e.preventDefault();
 
+    if (formIsInvalid()) {
+      return;
+    }
+
+    const getValues = () => {
+      const [firstName, lastName, email, message] = formFields;
+      return {
+        firstName: firstName.value.trim(),
+        lastName: lastName.value.trim(),
+        emailAddress: email.value.trim(),
+        message: message.value.trim(),
+      };
+    };
     const { firstName, lastName, emailAddress, message } = getValues();
     const request = new Request(formTargetUrl, {
       method: 'POST',
       body: JSON.stringify({ firstName, lastName, emailAddress, message }),
       headers: { 'Content-Type': 'application/json' },
     });
+
+    const setFormDisable = (disabled) => {
+      submitButton.disabled = disabled;
+      formFields.forEach(field => field.disabled = disabled);
+    };
+    const disableForm = () => setFormDisable(true);
+    const enableForm = () => setFormDisable(false);
+
+    statusMessageElement.innerText = '';
+    statusMessageElement.classList.remove('error');
+    disableForm();
     fetch(request)
-      .then(() => {
-        contactForm.reset();
+      .then(response => {
+        if (response.ok) {
+          contactForm.reset();
+          submitButton.innerText = submitButton.getAttribute('data-success-text');
+          submitButton.classList.add('success');
+
+        } else {
+          enableForm();
+          statusMessageElement.classList.add('error');
+          let figuredOutError = false;
+
+          if (response.status === 400) {
+            response.text()
+              .then(errorMessage => {
+                const parts = errorMessage.split('/');
+                if (parts.length === 2 && parts[0] === 'validation') {
+                  const erroredField = parts[1];
+                  switch (erroredField) {
+                    case 'firstName':
+                    case 'lastName': {
+                      statusMessageElement.innerText = statusMessageElement.getAttribute('data-name-validation-text');
+                      makeInputErrored(firstNameElement);
+                      makeInputErrored(lastNameElement);
+                      figuredOutError = true;
+                      break;
+                    }
+                    case 'emailAddress': {
+                      statusMessageElement.innerText = statusMessageElement.getAttribute('data-email-validation-text');
+                      makeInputErrored(emailElement);
+                      figuredOutError = true;
+                      break;
+                    }
+                    case 'message': {
+                      statusMessageElement.innerText = statusMessageElement.getAttribute('data-message-validation-text');
+                      makeInputErrored(messageElement);
+                      figuredOutError = true;
+                      break;
+                    }
+                  }
+                }
+              })
+              .catch(err => console.error(err))
+              .finally(() => {
+                if (!figuredOutError) {
+                  statusMessageElement.innerText = statusMessageElement.getAttribute('data-generic-error-text');
+                }
+              });
+          }
+        }
+      })
+      .catch(err => {
+        enableForm();
+        statusMessageElement.classList.add('error');
+        statusMessageElement.innerText = statusMessageElement.getAttribute('data-generic-error-text');
+        console.error(err);
       });
   });
 });
